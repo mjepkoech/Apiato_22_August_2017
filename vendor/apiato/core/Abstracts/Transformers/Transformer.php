5f9f50764eecb15fae2b60ecfb291705279095ed
vagrant <vagrant@homestead>
2017-08-22 05:46:20 +0000

    Initial commit

---
+++ vendor/apiato/core/Abstracts/Transformers/Transformer.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Apiato\Core\Abstracts\Transformers;
+
+use App\Containers\Authentication\Tasks\GetAuthenticatedUserTask;
+use Illuminate\Support\Facades\App;
+use League\Fractal\TransformerAbstract as FractalTransformer;
+
+/**
+ * Class Transformer.
+ *
+ * @author  Mahmoud Zalt <mahmoud@zalt.me>
+ */
+abstract class Transformer extends FractalTransformer
+{
+
+    /**
+     * @return  mixed
+     */
+    public function user()
+    {
+        return App::make(GetAuthenticatedUserTask::class)->run();
+    }
+
+    /**
+     * @param $adminResponse
+     * @param $clientResponse
+     *
+     * @return  array
+     */
+    public function ifAdmin($adminResponse, $clientResponse)
+    {
+        $user = $this->user();
+
+        if (!is_null($user) && $user->hasAdminRole()) {
+            return array_merge($clientResponse, $adminResponse);
+        }
+
+        return $clientResponse;
+    }
+
+    /**
+     * @param mixed $data
+     * @param callable|FractalTransformer $transformer
+     * @param null $resourceKey
+     *
+     * @return \League\Fractal\Resource\Item
+     */
+    public function item($data, $transformer, $resourceKey = null)
+    {
+        // set a default resource key if none is set
+        if(!$resourceKey && $data) {
+            $resourceKey = $data->getResourceKey();
+        }
+
+        return parent::item($data, $transformer, $resourceKey);
+    }
+
+    /**
+     * @param mixed $data
+     * @param callable|FractalTransformer $transformer
+     * @param null $resourceKey
+     *
+     * @return \League\Fractal\Resource\Collection
+     */
+    public function collection($data, $transformer, $resourceKey = null)
+    {
+        // set a default resource key if none is set
+        if(!$resourceKey && $data->isNotEmpty()) {
+            $obj = $data->first();
+            $resourceKey = $obj->getResourceKey();
+        }
+
+        return parent::collection($data, $transformer, $resourceKey);
+    }
+
+}
